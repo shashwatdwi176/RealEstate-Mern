f0b8511751f2371b2720b165ecd20d90dd9bc5db
shashwatdwi176 <dwivedishashwat176@gmail.com>
2024-03-18 21:23:16 +0530

    Added Home Page

---
+++ client/src/App.jsx
@@ -0,0 +1,28 @@
+import Header from "./component/Header/Header";
+import Hero from "./component/Hero/Hero";
+import "./App.css";
+import Companies from "./component/Companies/Companies";
+import Residencies from "./component/Residencies/Residencies";
+import Value from "./component/Value/Value";
+import Contact from "./component/Contact/Contact";
+import GetStarted from "./component/GetStarted/GetStarted";
+import Footer from "./component/Footer/Footer";
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <div className="white-gradient" />
+        <Header />
+        <Hero />
+      </div>
+      <Companies />
+      <Residencies />
+      <Value />
+      <Contact />
+      <GetStarted />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
